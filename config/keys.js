b18df3cb00a32cd3feca4aa997777603488d4953
lav-joshi <lcs2019022@iiitl.ac.in>
2021-04-30 02:08:25 +0530

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-    mongoURI: "mongodb://lav_joshi:lavjoshi1056@cluster0-shard-00-00.qo22l.mongodb.net:27017,cluster0-shard-00-01.qo22l.mongodb.net:27017,cluster0-shard-00-02.qo22l.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mpp6xq-shard-0&authSource=admin&retryWrites=true&w=majority",
-    jwtSecret :"lav joshi",
-    clientId: "428185779294-4bue4hiho7mqp2866jqs9vnugv7uskan.apps.googleusercontent.com"
-};
+
+if(process.env.NODE_ENV =="production"){
+    module.exports = require("./prod");
+}else{
+    module.exports = require("./dev");
+}
